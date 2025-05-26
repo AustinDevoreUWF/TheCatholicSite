c08f618a2bdc758d94ae716ddf676f91691a0a5f
AustinDevoreUWF <ard78@students.uwf.edu>
2025-05-26 16:16:31 -0500

    Added Jsons

--- html/script.js
+++ html/script.js
@@ -1,3 +1,9 @@
-function dailySaint(){
-const saintName = 
+async function getRandomSaint(){
+const response = await fetch('data/Saints.json');
+const saints = await response.json();
+const index = saints[Math.floor(Math.random() * saints.length)];
+return saints[index].name;
+}
+async function getFromWiki(saintName){
+const url = 
 }
